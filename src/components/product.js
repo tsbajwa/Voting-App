40e6b7758c18f08954f4d5bfe24ed90933520f07
tsbajwa <teranjit@gmail.com>
2017-07-31 13:44:59 +1000

    initial styling for product component

--- src/components/product.js
+++ src/components/product.js
@@ -1,21 +1,20 @@
 import React from 'react';
 
 export default function Product(props) {
-  const productContainer = {
-    display: 'flex',
-  }
   return (
-    <div style={productContainer}>
-      <div>
-        <img src={require('../assets/products/image-aqua.png')} alt='product'></img>
+    <div className='product'>
+      <div className='product-img'>
+        <img className='product-img__img'src={require('../assets/products/image-aqua.png')} alt='product'></img>
       </div>
-      <div>
-        <div>
-          <img src={require('../assets/upvote.png')} alt='vote up'></img> 27
+      <div className='product-info'>
+        <div className='product-upvote'>
+          <img className='product-upvote__img'src={require('../assets/upvote.png')} alt='vote up'></img> 27
         </div>
         <a href='#'>Tinfoild: Tailored tinfoil Hats</a>
         <p>We already have your measurements and shipping address</p>
-        <span>Submitted by:</span><img src={require('../assets/avatars/elliot.jpg')} alt='author'></img>
+        <div className='product-author'>
+          <span>Submitted by:</span><img className='product-author__img'src={require('../assets/avatars/elliot.jpg')} alt='author'></img>
+        </div>
       </div>
     </div>
   )
